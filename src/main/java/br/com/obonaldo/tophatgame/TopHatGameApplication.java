e5e541ccb1ee7f69550dc93b59f124d44a4f7458
Daniel Bonaldo <daniel.bonaldo@mercadolivre.com>
2020-05-26 17:57:26 -0300

    project bootstrap

---
+++ src/main/java/br/com/obonaldo/tophatgame/TopHatGameApplication.java
@@ -0,0 +1,13 @@
+package br.com.obonaldo.tophatgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopHatGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopHatGameApplication.class, args);
+	}
+
+}
